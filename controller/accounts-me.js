c3f4a9ac7093637a8c3cfdb0666be4ec926ec959
Joe Coyle <jcoyle37@gmail.com>
2019-06-05 15:28:11 -0400

    fixed JSON response to be more straightforward

--- controller/accounts-me.js
+++ controller/accounts-me.js
@@ -3,12 +3,11 @@ const AccountModel = require('../model/account');
 module.exports = (req, res, next) => {
   return AccountModel
     .find({
-      where: { 'email': req.email }, //primary key for user record
+      where: { 'email': req.email },
       limit: 1,
     })
     .then((list) => {
-      let modifiedList = Object.assign({}, list, {auth: true}); //add 'auth: true' to object
-      if(list.length) res.send(modifiedList);
+      if(list.length) res.send({auth: true, userData: list[0]});
       else res.send({ status: 'user not found'});
     })
     .catch(next);
